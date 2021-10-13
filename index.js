2b56750fefbe5a90d01c75e557003baf02976b7c
KVCobbs <kenyacobbs9001@gmail.com>
2021-10-13 13:58:01 +0000

    did as much as I could

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 //1
 function sumOfThreeAndFive(){
-  
+  let sum = 0; 
+  for(let i = 1; i <= 1000; i++) {
+      if(i % 3 === 0 || i % 5 === 0) {
+          sum += i;
+      }
+      
+  }
+  return sum;
 }
 
-function greaterNum(){
-  
+function greaterNum(x, y){
+  if(x > y) {
+      return x;
+  } if (y > x) {
+      return y;
+  } else if (y == x ) {
+      return("these numbers are the same");
+  }
 }
 
 function oddAndEvenInFifteen(){
@@ -18,5 +31,6 @@ function sortThreeNums(){
 
 
 
-
+//console.log(sumOfThreeAndFive());
+console.log(greaterNum(15, 15));
 
